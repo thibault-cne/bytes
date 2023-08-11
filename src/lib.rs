bc12bab57ac153d6d428abd2c70c495b14ba2418
thibault <thibault.chene23@gmail.com>
2023-08-11 10:17:03 +0200

    [~] change filename

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,11 @@ extern crate alloc;
 extern crate std;
 
 mod buf;
-mod byte_str;
 mod bytes;
+mod bytes_str;
 mod fmt;
 
-pub use crate::byte_str::BytesStr;
 pub use crate::bytes::Bytes;
+pub use crate::bytes_str::BytesStr;
 
 pub use crate::buf::{Buf, BufMut};
